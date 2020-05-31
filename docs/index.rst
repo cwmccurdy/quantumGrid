11166266e813f980ea57b3cfeb8c88485966ef5d
zstreeter <zstreeter@ucdavis.edu>
2020-05-31 14:17:42 -0700

    initial commit

--- docs/index.rst
+++ docs/index.rst
@@ -1,16 +1,5 @@
-.. raw:: html
-
-   <h1 align="center">
-     💥scatterGrid💥
-   </h1>
-
-.. raw:: html
-
-   <p align="center">
-     <img src=_static/scatteredWave.png width="500px;" height="400px;" alt=""/>
-   </p>
-
-Brought to you by the AMO theory group at Berkeley National Lab.
+Welcome to scatterGrid's documentation!
+======================================
 
 .. toctree::
    :maxdepth: 2
--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ with open('HISTORY.rst') as history_file:
 
 requirements = ['Click>=7.0', ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [ ]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [ ]
 
 setup(
     author="Zachary Streeter",
@@ -31,8 +31,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    description="Exterior Complex Scaled Finite-Element Element Discrete \
-                 Variable Representation grid for general physics problems.",
+    description="Exterior Complex Scaled Finite-Element Method Discrete Variable Representation grid for general physics problems.",
     entry_points={
         'console_scripts': [
             'scattergrid=scattergrid.cli:main',
