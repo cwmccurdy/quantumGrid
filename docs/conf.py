f8dee560dc784f4e71d6ca9084c31770354b6531
zstreeter <zstreeter@ucdavis.edu>
2020-05-31 11:49:56 -0700

    docs work locally

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 #
-import scattergrid
+# import scattergrid
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
