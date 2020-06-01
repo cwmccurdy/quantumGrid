ff82a4908227aeb285a4cadfec7898e4b3610638
zstreeter <zstreeter@ucdavis.edu>
2020-05-31 19:53:43 -0700

    nan

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
