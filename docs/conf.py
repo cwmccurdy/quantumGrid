e2de312011828d590b8d181333bc4e3eef1a12b1
zstreeter <zstreeter@ucdavis.edu>
2020-05-31 21:11:14 -0700

    trying to mess with sys path in conf

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import scattergrid
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..','../scattergrid/'))
+sys.path.insert(0, os.path.abspath('../scattergrid/'))
 
 # -- General configuration ---------------------------------------------
 
