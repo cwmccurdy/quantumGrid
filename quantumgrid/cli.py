59f4727e6c1497494b2f941c77a1076cdd95c9dd
zstreeter <zstreeter@ucdavis.edu>
2020-10-13 17:34:23 -0500

    added back cli.py for travis test build

---
+++ quantumgrid/cli.py
@@ -0,0 +1,15 @@
+"""Console cript for quantumgrid."""
+import sys
+import click
+
+@click.command()
+def main(args=None):
+    """Console script for quantumGrid."""
+    click.echo("Replace this message by putting your code into "
+               "quantumgrid.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no covers
--- tests/test_quantumgrid.py
+++ tests/test_quantumgrid.py
@@ -6,7 +6,7 @@ import pytest
 
 from click.testing import CliRunner
 
-from quantumgrid import quantumgrid
+from quantumgrid import femdvr
 from quantumgrid import cli
 
 
@@ -31,7 +31,7 @@ def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'quantumgrid.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    assert "quantumgrid.cli.main" in result.output
+    help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert "--help  Show this message and exit." in help_result.output
