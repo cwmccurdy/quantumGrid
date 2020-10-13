facfd6cbc488aa4817efb2c669de6a21c52471cf
zstreeter <zstreeter@ucdavis.edu>
2020-10-13 17:34:55 -0500

    nan

--- quantumgrid/cli.py
+++ quantumgrid/cli.py
@@ -2,11 +2,13 @@
 import sys
 import click
 
+
 @click.command()
 def main(args=None):
     """Console script for quantumGrid."""
-    click.echo("Replace this message by putting your code into "
-               "quantumgrid.cli.main")
+    click.echo(
+        "Replace this message by putting your code into " "quantumgrid.cli.main"
+    )
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
