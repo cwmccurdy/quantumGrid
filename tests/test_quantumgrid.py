3a2fd38781458a5a072fae698a9e044da17b54e7
zstreeter <zstreeter@ucdavis.edu>
2020-10-15 13:05:04 -0500

    small change to test

--- tests/test_quantumgrid.py
+++ tests/test_quantumgrid.py
@@ -33,4 +33,4 @@ def test_command_line_interface():
     assert "quantumgrid.cli.main" in result.output
     help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
-    assert "--help  Show this message and exit." in help_result.outputl
+    assert "--help  Show this message and exit." in help_result.output
