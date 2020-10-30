84905a2e83b53e3de4dccf1dbfa285aded81db49
zstreeter <zstreeter@ucdavis.edu>
2020-10-30 09:15:06 -0500

    added newer version to pypi

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/zstreeter/quantumgrid",
-    version="0.0.2",
+    version="0.0.3",
     zip_safe=False,
 )
