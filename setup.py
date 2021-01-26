c055adb99177af01f0f93d927d61903a9d9339b7
ztreeter <zacharylouis42@gmail.com>
2021-01-26 16:03:53 -0600

    changed python_requires=>3.7 in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 setup(
     author="Zachary Streeter",
     author_email="zlstreeter@lbl.gov",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
